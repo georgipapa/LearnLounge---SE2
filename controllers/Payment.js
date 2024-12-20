9afce1d2eddd9060a1cb688a32abc299a5cfe254
psigkoud <psigkoud@ece.auth.gr>
2024-12-20 23:17:00 +0200

    rightparameters

--- controllers/Payment.js
+++ controllers/Payment.js
@@ -1,14 +1,26 @@
 'use strict';
 
-var utils = require('../utils/writer.js');
-var Payment = require('../service/PaymentService');
+const utils = require('../utils/writer.js');
+const Payment = require('../service/PaymentService');
 
-module.exports.courses_course_id_pay_post = function courses_course_id_pay_post (req, res, next, body, courseId) {
-  Payment.courses_course_id_pay_post(body, courseId)
-    .then(function (response) {
-      utils.writeJson(res, response);
-    })
-    .catch(function (response) {
-      utils.writeJson(res, response);
-    });
+/**
+ * Utility function to handle response promises
+ * @param {Object} res - Response object
+ * @param {Promise} promise - The promise to handle
+ */
+const handleResponse = (res, promise) => {
+  promise
+    .then(response => utils.writeJson(res, response))
+    .catch(error => utils.writeJson(res, error));
+};
+
+/**
+ * Handles payment for a specific course
+ * @param {Object} params - The parameters for the request
+ * @param {Object} params.res - Response object
+ * @param {Object} params.body - Request body
+ * @param {string} params.courseId - Course ID
+ */
+module.exports.courses_course_id_pay_post = function courses_course_id_pay_post({ res, body, courseId }) {
+  handleResponse(res, Payment.courses_course_id_pay_post(body, courseId));
 };
